719ff6efcfc05cb1d516e598631e645417a29a45
Stepan <stepan.gavriusenco@gmail.com>
2023-10-09 16:35:58 +0300

    v2.0

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,3 +15,46 @@
 
     <button type="submit">Создать задачу</button>
 </form>
+<style>
+
+form {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+
+label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="number"],
+input[type="date"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    font-size: 16px;
+}
+
+button[type="submit"] {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+button[type="submit"]:hover {
+    background-color: #0056b3;
+}
+</style>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,7 +4,7 @@
     <input type="hidden" name="task_id" value="{{ $task->id }}">
     <label for="title">title:</label>
     <input type="text" name="title" value="{{ $task->title }}" required>
-    что за хуйяня хвахвахв
+
     <label for="description">description</label>
     <input type="text" name="description" value="{{ $task->description }}" required>
 
@@ -16,3 +16,48 @@
 
     <button type="submit">Edit задачу</button>
 </form>
+<style>
+    form {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+
+/* Стили для меток (labels) */
+label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+/* Стили для полей ввода (input) */
+input[type="text"],
+input[type="number"],
+input[type="date"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    font-size: 16px;
+}
+
+/* Стили для кнопки редактирования (button) */
+button[type="submit"] {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+button[type="submit"]:hover {
+    background-color: #0056b3;
+}
+</style>
